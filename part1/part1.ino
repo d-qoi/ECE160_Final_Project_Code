7da294366b3a471cabb38c79d47674f44bf0242f
Alex Hirschfeld <alex@d-qoi.com>
2015-10-13 11:23:31 -0400

    Part one

---
+++ part1/part1.ino
@@ -0,0 +1,28 @@
+/*
+ * Coder: Alexander Hirschfeld
+ * 
+ * Date: Oct. 13, 2015
+ * 
+ * Part 1
+ */
+ 
+byte redLED = 10;
+byte redButton = 9;
+byte greenLED = 3;
+byte greenButton = 2;
+byte blueLED = 13;
+byte blueButton = 12;
+byte yellowLED = 5;
+byte yellowButton = 6;
+
+byte buzzerOne = 7;
+byte buzzerTwo = 4;
+
+void setup() {
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
